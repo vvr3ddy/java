b8cbe82101fbc941a65563d02af2c84c42e03cf4
Vishnuvardhan Reddy <39486314+Quantummech2000@users.noreply.github.com>
2020-01-28 14:36:37 +0530

    Basics : create helloWorld.java

---
+++ Basics/helloWorld.java
@@ -0,0 +1,14 @@
+/*
+// Copyrights (C) VVR 2020.
+// Learning Java by Programming OTG
+// File Name           : helloWorld.java
+// Author              : Vishnuvardhan Reddy
+// Description of File : This begins my journey through Java programming language.
+                        
+*/
+
+public class hellWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
