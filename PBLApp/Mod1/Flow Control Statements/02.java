bdbc598aacbbcd0033351fafbe395e79cb4c25af
Vishnuvardhan Reddy <39486314+vvr3ddy@users.noreply.github.com>
2020-07-09 18:21:58 +0530

    :bug: fix derp
    close main method and fix indentations

--- PBLApp/Mod1/Flow Control Statements/02.java
+++ PBLApp/Mod1/Flow Control Statements/02.java
@@ -4,8 +4,9 @@ public class Mod1 {
 	public static void main(String[] args) {
 		Scanner read = new Scanner(System.in);
 		int a = read.nextInt();
-    if(a%2 == 0)
-      System.out.println("Even");
-     else
-      System.out.println("Odd");
+    		if(a%2 == 0)
+      			System.out.println("Even");
+     		else
+      			System.out.println("Odd");
+	}
 }
