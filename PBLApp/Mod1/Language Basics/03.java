6d4f8b3660815aafdf7c67100e0931855ed2e799
Vishnuvardhan Reddy <39486314+vvr3ddy@users.noreply.github.com>
2020-07-09 18:08:30 +0530

    :sparkles: PBLApp/Mod1/Basics: Create 03.java
    WAP to compute the sum of two integer arguements.

---
+++ PBLApp/Mod1/Language Basics/03.java
@@ -0,0 +1,9 @@
+public class Mod1 {
+
+	public static void main(String[] args) {
+    int arg1 = Integer.parseInt(args[0]);
+    int arg2 = Integer.parseInt(args[1]);
+    int sum = arg1+arg2;
+		System.out.println(sum);
+	}
+}
