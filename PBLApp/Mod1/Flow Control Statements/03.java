35e729a0e82d69796f610ac15e1a9d67444489cc
Vishnuvardhan Reddy <39486314+vvr3ddy@users.noreply.github.com>
2020-07-09 18:26:36 +0530

    :sparkles: PBLApp/Mod1/Flow.. : Create 03.java
    WAP to check if a program has received any command line arguements or not.
    If the program has not received any arguements print "No Values" else print all the elements separated by a comma.

---
+++ PBLApp/Mod1/Flow Control Statements/03.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Mod1 {
+
+	public static void main(String[] args) {
+    if(args.length == 0)
+      System.out.println("No Values");
+    else{
+      System.out.print(args[0]);
+      for(int i = 1; i< args.length; i++){
+        System.out.print(", "+args[i]);
+      }
+    }
+	}
+
+}
