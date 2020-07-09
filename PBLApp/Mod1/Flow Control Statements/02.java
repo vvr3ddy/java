325c63637524732d29eafba6af18b6cb032fa567
Vishnuvardhan Reddy <39486314+vvr3ddy@users.noreply.github.com>
2020-07-09 18:20:26 +0530

    :sparkles: PBLApp/Mod1/Flow.. : Create 02.java
    WAP to check if a given integer is even or odd

---
+++ PBLApp/Mod1/Flow Control Statements/02.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Mod1 {
+
+	public static void main(String[] args) {
+		Scanner read = new Scanner(System.in);
+		int a = read.nextInt();
+    if(a%2 == 0)
+      System.out.println("Even");
+     else
+      System.out.println("Odd");
+}
