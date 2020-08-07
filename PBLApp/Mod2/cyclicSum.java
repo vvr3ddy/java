9cbe1dec34cca05c6ea7bb46d018d2cb4bdb4848
Vishnuvardhan Reddy <39486314+vvr3ddy@users.noreply.github.com>
2020-08-07 18:41:07 +0530

    :gear: Fix logic
    The assignment expects us to calculate the sum as follows for 1234 -> (1+2+3+4)+(2+3+4)+(3+4)+(4) = 30
    
    
    Attaching a reference output for the input 58420
    
    Inside cycSum..
    Current Digit in cycSum:5
    Inside sumOfDigits
    Initial Number:58420
    Initial Length:5
    
    Current Digit:5
    Current dig*pos:50000
    Current Length:4
    Current number:8420
    Current Sum:5
    
    Current Digit:8
    Current dig*pos:8000
    Current Length:3
    Current number:420
    Current Sum:13
    
    Current Digit:4
    Current dig*pos:400
    Current Length:2
    Current number:20
    Current Sum:17
    
    Current Digit:2
    Current dig*pos:20
    Current Length:1
    Current number:0
    Current Sum:19
    
    Current Digit:0
    Current dig*pos:0
    Current Length:0
    Current number:0
    Current Sum:19
    
    Out of sumOfDigits
    Final Sum:19
    ====================
    Cyclic Sum Value : 19
    ====================
    
    Current Digit in cycSum:8
    Inside sumOfDigits
    Initial Number:8420
    Initial Length:4
    
    Current Digit:8
    Current dig*pos:8000
    Current Length:3
    Current number:420
    Current Sum:8
    
    Current Digit:4
    Current dig*pos:400
    Current Length:2
    Current number:20
    Current Sum:12
    
    Current Digit:2
    Current dig*pos:20
    Current Length:1
    Current number:0
    Current Sum:14
    
    Current Digit:0
    Current dig*pos:0
    Current Length:0
    Current number:0
    Current Sum:14
    
    Out of sumOfDigits
    Final Sum:14
    ====================
    Cyclic Sum Value : 33
    ====================
    
    Current Digit in cycSum:4
    Inside sumOfDigits
    Initial Number:420
    Initial Length:3
    
    Current Digit:4
    Current dig*pos:400
    Current Length:2
    Current number:20
    Current Sum:4
    
    Current Digit:2
    Current dig*pos:20
    Current Length:1
    Current number:0
    Current Sum:6
    
    Current Digit:0
    Current dig*pos:0
    Current Length:0
    Current number:0
    Current Sum:6
    
    Out of sumOfDigits
    Final Sum:6
    ====================
    Cyclic Sum Value : 39
    ====================
    
    Current Digit in cycSum:2
    Inside sumOfDigits
    Initial Number:20
    Initial Length:2
    
    Current Digit:2
    Current dig*pos:20
    Current Length:1
    Current number:0
    Current Sum:2
    
    Current Digit:0
    Current dig*pos:0
    Current Length:0
    Current number:0
    Current Sum:2
    
    Out of sumOfDigits
    Final Sum:2
    Cyclic Sum :41
    41

--- PBLApp/Mod2/cyclicSum.java
+++ PBLApp/Mod2/cyclicSum.java
@@ -2,39 +2,77 @@ package M2;
 
 public class CyclicSum {
     int sumOfDigits(int number) {
-        int rem, dig, sum = 0;
+
+        int dig, sum = 0;
         int length = new String(String.valueOf(number)).length();
+        //debug statements start
+
         System.out.println("Inside sumOfDigits");
         System.out.println("Initial Number:" + number);
-        System.out.println("Iniial Length:" + length);
+        System.out.println("Initial Length:" + length);
         System.out.println();
-        while (length >= 0) {
-            int pos = (int) Math.pow(10, length);
+
+        //debug statements end
+        while (length > 0) {
+
+            int pos = (int) Math.pow(10, length - 1);
             dig = (number / pos);
+            //debug statement
+
             System.out.println("Current Digit:" + dig);
+
             sum += dig;
             number = number - (dig * pos);
+
+            System.out.println("Current dig*pos:" + dig * pos);
+
             length--;
+
+            //debug statements start
+
             System.out.println("Current Length:" + length);
             System.out.println("Current number:" + number);
             System.out.println("Current Sum:" + sum);
             System.out.println();
+
+            //debug statements end
         }
+
         System.out.println("Out of sumOfDigits");
         System.out.println("Final Sum:" + sum);
+
         return sum;
     }
 
     int cycSum(int number) {
+
         int cyclicSum = 0;
+        int length = new String(String.valueOf(number)).length();
+        int dig = 0;
+
         System.out.println("Inside cycSum..");
-        while (number > 0) {
+
+        while (length > 0) {
+
+            int pos = (int) Math.pow(10, length - 1);
+            dig = (number / pos);
+
+            System.out.println("Current Digit in cycSum:" + dig);
+
             cyclicSum += sumOfDigits(number);
-            number /= 10;
+            number = number - (dig * pos);
+            length--;
+
+            if (number == 0) {
+                break;
+            }
+
             System.out.println("====================");
-            System.out.println("Cyclic Sum Value : "+ cyclicSum);
+            System.out.println("Cyclic Sum Value : " + cyclicSum);
             System.out.println("====================");
+            System.out.println();
         }
+        System.out.println("Cyclic Sum :" + cyclicSum);
         return cyclicSum;
     }
 }
@@ -42,6 +80,6 @@ public class CyclicSum {
 class Main {
     public static void main(String[] args) {
         CyclicSum cs = new CyclicSum();
-        System.out.println(cs.cycSum(123));
+        System.out.println(cs.cycSum(58420));
     }
 }
