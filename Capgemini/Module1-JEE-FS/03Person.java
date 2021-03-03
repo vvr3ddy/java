60539d0fa03d48c9c927c85c5cce8c5d46469c46
Vishnuvardhan Reddy <39486314+vvr3ddy@users.noreply.github.com>
2021-03-03 17:39:11 +0530

    :octocat: Capgemini/Module1-JEE-FS : Create 03Person.java

---
+++ Capgemini/Module1-JEE-FS/03Person.java
@@ -0,0 +1,46 @@
+import java.util.stream.Collectors;
+class Person {
+	private String name;
+	private int age;
+
+	public Person(String n, int a) {
+		this.name = n;
+		this.age = a;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
+
+class StreamImplementation {
+	public int summationOfAge(List<Person> list) {
+		Integer sumOfAge = list.stream().map(Person::getAge).filter(age -> age > 50).reduce(0, Integer::sum);
+		return sumOfAge;
+	}
+
+	public List<String> getPersonName(List<Person> list) {
+		List<String> names = list.stream().map(Person::getName).collect(Collectors.toList());
+
+		return names;
+	}
+
+	public List<Integer> getPersonAge(List<Person> list) {
+		List<Integer> ages = list.stream().map(Person::getAge).collect(Collectors.toList());
+
+		return ages;
+	}
+
+}
