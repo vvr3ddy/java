dc93a148a08c8367f2c52ab552f5beeb80444d75
Vishnuvardhan Reddy <39486314+vvr3ddy@users.noreply.github.com>
2021-03-03 17:40:08 +0530

    :octocat: Capgemini/Module1-JEE-FS : Create 01Source.java

---
+++ Capgemini/Module1-JEE-FS/01Source.java
@@ -0,0 +1,44 @@
+class Source {
+	public static int sum(ArrayList<Integer> numbers) {
+		int sum = 0;
+		for (int i : numbers) {
+			sum += i;
+		}
+		return sum;
+	}
+
+	public static ArrayList<Integer> splitAndReverse(ArrayList<Integer> list) {
+		int size = list.size();
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		ArrayList<Integer> b = new ArrayList<Integer>();
+		ArrayList<Integer> res = new ArrayList<Integer>();
+		if (size % 2 == 0) {
+			for (int i = 0; i < size / 2; i++) {
+				a.add(list.get(i));
+			}
+			for (int i = size / 2; i < size; i++) {
+				b.add(list.get(i));
+			}
+		} else {
+			for (int i = 0; i < size / 2 + 1; i++) {
+				a.add(list.get(i));
+			}
+			for (int i = size / 2 + 1; i < size; i++) {
+				b.add(list.get(i));
+			}
+		}
+
+		Collections.reverse(a);
+		Collections.reverse(b);
+
+		res.addAll(a);
+		res.addAll(b);
+		return res;
+
+	}
+
+	public Integer getItemAtIndex(ArrayList<Integer> list, int index) {
+		return list.get(index);
+
+	}
+}
